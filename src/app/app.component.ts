bd77e47f30fc31cd589c363a7fc417349f9b8cd1
see-tha <233757@ust-global.com>
2022-10-14 14:34:25 +0530

    7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo3';
+  
+  list:any[]=[];
+  
+  EnterSubmit(item:String,bo:any)
+    {
+      
+      if(item!="")
+this.list.push({name:item});
+bo.value=""
+
+}
+  remove(i:number)
+  {
+this.list.splice(i,1);
+  }
+
+
 }
